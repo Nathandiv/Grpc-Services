1dc330b6e10523ca8cf53f71f738623875f74676
Nathan Hlungwani <k.n.hlungwani@gmail.com>
2024-10-23 20:20:56 +0200

    adding grpc files which i created

---
+++ AwesomeClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using awesomeServices;
+using Grpc.Net.Client;
+
+Console.WriteLine("please enter a name:");
+var name = Console.ReadLine();
+
+using var channel = GrpcChannel.ForAddress ("http://localhost:7139");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest{Name =name});
+Console.WriteLine("Greeting:"+reply.Message);
+Console.ReadKey();
+{
+    
+}
